d4f982064d7e471e819ff5665946ef1e6610ea77
Ayoyinka2456 <108306542+Ayoyinka2456@users.noreply.github.com>
2024-03-21 01:13:31 -0400

    Delete clone_single.sh

--- clone_single.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone -b test --single-branch https://github.com/Ayoyinka2456/Devops_project.git temp_folder
-cd temp_folder
-git archive HEAD -- increment_counter.sh | tar -x
-mv increment_counter.sh ..
-cd ..
-rm -rf temp_folder
-source increment_counter.sh
