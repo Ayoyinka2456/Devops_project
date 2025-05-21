6abfd1e7d74a032a1e00136d3db0dce0317a7857
Ayoyinka2456 <108306542+Ayoyinka2456@users.noreply.github.com>
2025-05-21 05:57:53 -0400

    Update ansible-k8s-workstation.sh

--- ansible-k8s-workstation.sh
+++ ansible-k8s-workstation.sh
@@ -6,11 +6,11 @@ ansible-playbook -i host.ini install_tools.yml
 
 #create cluster
 
-kops create cluster \
-  --name=final-project-1-k8s-cluster.local \
-  --state=s3://final-project-1-k8s-store \
-  --zones=us-east-2a,us-east-2b \
-  --node-count=2 \
-  --node-size=t2.medium \
-  --control-plane-size=t2.medium \
-  --ssh-public-key="~/.ssh/id_rsa.pub"
+# kops create cluster \
+#   --name=final-project-1-k8s-cluster.local \
+#   --state=s3://final-project-1-k8s-store \
+#   --zones=us-east-2a,us-east-2b \
+#   --node-count=2 \
+#   --node-size=t2.medium \
+#   --control-plane-size=t2.medium \
+#   --ssh-public-key="~/.ssh/id_rsa.pub"
