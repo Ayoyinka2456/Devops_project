1c3ffbfee21b65320bdd47b3c26228a3b9511d32
Ayoyinka2456 <108306542+Ayoyinka2456@users.noreply.github.com>
2025-05-22 03:15:20 -0400

    Update ansible-k8s-workstation.sh

--- ansible-k8s-workstation.sh
+++ ansible-k8s-workstation.sh
@@ -1,18 +1,3 @@
 
 cd k8s-admin-setup
 ansible-playbook -i host.ini install_tools.yml
-
-
-#ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -C "kops-key"
-#aws s3 cp ~/.ssh/id_rsa.pub s3://final-project-1-k8s-store/kops-key.pub
-
-#create cluster
-
-# kops create cluster \
-#   --name=final-project-1-k8s-cluster.local \
-#   --state=s3://final-project-1-k8s-store \
-#   --zones=us-east-2a,us-east-2b \
-#   --node-count=2 \
-#   --node-size=t2.medium \
-#   --control-plane-size=t2.medium \
-#   --ssh-public-key="~/.ssh/id_rsa.pub"
