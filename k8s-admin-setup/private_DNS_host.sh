50322e11dc118163405120b37adae7138ad3c30c
Ayoyinka2456 <108306542+Ayoyinka2456@users.noreply.github.com>
2025-05-22 00:32:19 -0400

    Update private_DNS_host.sh

--- k8s-admin-setup/private_DNS_host.sh
+++ k8s-admin-setup/private_DNS_host.sh
@@ -1,7 +1,7 @@
-VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=final-project-1-vpc" --query "Vpcs[0].VpcId" --output text)
+# VPC_ID=$(aws ec2 describe-vpcs --filters "Name=tag:Name,Values=final-project-1-vpc" --query "Vpcs[0].VpcId" --output text)
 
-aws route53 create-hosted-zone \
-  --name final-project-1-k8s-cluster.local \
-  --caller-reference "$(date +%s)" \
-  --hosted-zone-config PrivateZone=true \
-  --vpc VPCRegion=us-east-2,VPCId=$VPC_ID
+# aws route53 create-hosted-zone \
+#  --name final-project-1-k8s-cluster.local \
+#  --caller-reference "$(date +%s)" \
+#  --hosted-zone-config PrivateZone=true \
+#  --vpc VPCRegion=us-east-2,VPCId=$VPC_ID
